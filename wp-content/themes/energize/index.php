6b76af83c20775b14b001c2e3654f21d64b7ffee
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2019-06-25 15:34:57 +0200

    test color

---
+++ wp-content/themes/energize/index.php
@@ -0,0 +1,3 @@
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed doloribus exercitationem inventore excepturi incidunt praesentium unde ullam ea voluptatibus? Fugit sit doloribus rerum, asperiores amet nesciunt illo iste atque dolores?
+</p>
